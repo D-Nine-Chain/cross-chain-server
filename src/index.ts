af6817019c7a2ce289eb4d3c82cdc3fd6deffd50
tangldev <tech@tangl.dev>
2023-12-15 22:19:17 +0800

    integrated cors

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,9 @@ import { getNodeD9Address } from "./d9/wallet";
 import { setupDb } from "./storage/firestoreSetup";
 export const app = express();
 export const d9Keyring = new Keyring({ type: 'sr25519' });
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 defineRoutes(app);
 setupDb().catch(console.error);
